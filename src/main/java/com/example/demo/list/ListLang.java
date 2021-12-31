d3e1c51b05492e915d73ef9e0bc8e3b181ead69c
lizenghai <fxmaoyuzzz@126.com>
2021-12-31 17:26:31 +0800

    20211231

--- src/main/java/com/example/demo/list/ListLang.java
+++ src/main/java/com/example/demo/list/ListLang.java
@@ -1,6 +1,8 @@
 package com.example.demo.list;
 
 import cn.hutool.core.collection.ListUtil;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
  * @Date: 2021/12/31
  * @Description:
  */
+@Slf4j
 public class ListLang {
 
     public static void main(String[] args) {
@@ -19,6 +22,7 @@ public class ListLang {
         for (int i = 0; i < 10; i++) {
             integers.add(i);
         }
+        log.info("list:{}", JSON.toJSONString(integers));
         List<List<Integer>> split = ListUtil.split(integers, 4);
         System.out.println(split);
     }
