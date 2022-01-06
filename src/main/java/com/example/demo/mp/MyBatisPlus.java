1031e4f1efa6c0ec987db8d8737dfb0ef85a1446
lizenghai <fxmaoyuzzz@126.com>
2022-01-06 19:02:58 +0800

    20220106

---
+++ src/main/java/com/example/demo/mp/MyBatisPlus.java
@@ -0,0 +1,15 @@
+package com.example.demo.mp;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.entity.Able;
+
+/**
+ * @author: zenghai.li
+ * @Date: 2022/01/06
+ */
+public class MyBatisPlus {
+
+
+    public static void main(String[] args) {
+    }
+}
