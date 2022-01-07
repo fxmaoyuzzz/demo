d31f897b3a201d3d9fffb84955ed08225dafb4af
lizenghai <fxmaoyuzzz@126.com>
2022-01-07 15:59:59 +0800

    20220107

---
+++ src/main/java/com/example/demo/map/Map.java
@@ -0,0 +1,28 @@
+package com.example.demo.map;
+
+import cn.hutool.core.lang.hash.Hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author: zenghai.li
+ * @Date: 2022/01/07
+ */
+public class Map {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "hahaha");
+        map.put("sum", 0);
+        ArrayList<java.util.Map<String, Object>> list = new ArrayList<>();
+        list.add(map);
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        list.forEach(item -> hashMap.put(item.get("name").toString(), (Integer) item.get("sum")));
+        System.out.println(hashMap);
+    }
+}
