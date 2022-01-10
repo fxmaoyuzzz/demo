267d2288571b568eab5c50e43e7db026abf573de
lizenghai <fxmaoyuzzz@126.com>
2022-01-10 13:22:41 +0800

    20220110

--- src/main/java/com/example/demo/number/NumberDemo.java
+++ src/main/java/com/example/demo/number/NumberDemo.java
@@ -1,5 +1,7 @@
 package com.example.demo.number;
 
+import cn.hutool.core.util.NumberUtil;
+
 /**
  * @author: zenghai.li
  * @Date: 2021/12/30
@@ -12,5 +14,10 @@ public class NumberDemo {
 
         Double b = a.doubleValue();
         System.out.println(b);
+
+        double c = 10;
+        double d = 3;
+        double div = NumberUtil.div(d, c);
+        System.out.println(div);
     }
 }
