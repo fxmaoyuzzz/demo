5b995f67cb0a5da4496a90a55a86534cfa3ca29f
lizenghai <fxmaoyuzzz@126.com>
2022-01-11 10:57:45 +0800

    20220111

--- src/main/java/com/example/demo/number/NumberDemo.java
+++ src/main/java/com/example/demo/number/NumberDemo.java
@@ -1,6 +1,9 @@
 package com.example.demo.number;
 
 import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.StrUtil;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author: zenghai.li
@@ -10,19 +13,22 @@ import cn.hutool.core.util.NumberUtil;
 public class NumberDemo {
 
     public static void main(String[] args) {
-        Integer a = 0;
-
-        Double b = a.doubleValue();
-        System.out.println(b);
-
-        double c = 10;
-        double d = 3;
-
-        double div = NumberUtil.div(d, c);
-
-        System.out.println(div);
+        //Integer a = 0;
+        //
+        //Double b = a.doubleValue();
+        //System.out.println(b);
+        //
+        //double c = 10;
+        //double d = 3;
+        //
+        //double div = NumberUtil.div(d, c);
+        //
+        //System.out.println(div);
 
     //    merge test
     //
+        String a = "[";
+        String sub = StrUtil.sub(a, 1, -1);
+        System.out.println(sub);
     }
 }
