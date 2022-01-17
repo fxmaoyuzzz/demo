ae4d0e9f6a19799faf03fe0384c42b8521bcca47
lizenghai <fxmaoyuzzz@126.com>
2022-01-17 16:39:28 +0800

    20220117

--- src/main/java/com/example/demo/number/NumberDemo.java
+++ src/main/java/com/example/demo/number/NumberDemo.java
@@ -5,6 +5,12 @@ import cn.hutool.core.util.StrUtil;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author: zenghai.li
  * @Date: 2021/12/30
@@ -27,8 +33,27 @@ public class NumberDemo {
 
     //    merge test
     //
-        String a = "[";
-        String sub = StrUtil.sub(a, 1, -1);
-        System.out.println(sub);
+    //    String a = "[";
+    //    String sub = StrUtil.sub(a, 1, -1);
+    //    System.out.println(sub);
+
+        //double a = 1.23756;
+        //String s = NumberUtil.roundStr(a, 2);
+        //a = Double.parseDouble(s);
+        //System.out.println();
+        //BigDecimal bigDecimal = new BigDecimal(2.34567);
+        //BigDecimal scale = bigDecimal.setScale(3, BigDecimal.ROUND_HALF_UP);
+        //System.out.println(scale);
+
+        ArrayList<Double> doubles = new ArrayList<>();
+        doubles.add(1.343d);
+        doubles.add(2.794d);
+        doubles.add(3.527d);
+        doubles.add(4.016d);
+        //List<String> collect = doubles.stream().map(item -> NumberUtil.roundStr(item, 1)).collect(Collectors.toList());
+        for (Double item : doubles) {
+            item = Double.valueOf(NumberUtil.roundStr(item, 2));
+        }
+        System.out.println(doubles);
     }
 }
