c74c884056d33c06bd730b1367513fd36872c148
lizenghai <fxmaoyuzzz@126.com>
2022-04-14 09:50:31 +0800

    20220414

---
+++ src/main/java/com/example/demo/time/Times.java
@@ -0,0 +1,41 @@
+package com.example.demo.time;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: zenghai.li
+ * @Description:
+ * @Date: 2022/04/13
+ */
+public class Times {
+
+    public static void main(String[] args) {
+        List<String> days = new ArrayList<>();
+        try {
+
+            String startDay = "2022-04-13";
+            String endDay = "2022-04-13";
+
+            DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+            Date startDate = dateFormat.parse(startDay);
+            Date endDate = dateFormat.parse(endDay);
+            Calendar tempStart = Calendar.getInstance();
+            tempStart.setTime(startDate);
+            Calendar tempEnd = Calendar.getInstance();
+            tempEnd.setTime(endDate);
+            tempEnd.add(Calendar.DATE, +1);// 日期加1(包含结束)
+            while (tempStart.before(tempEnd)) {
+                days.add(dateFormat.format(tempStart.getTime()));
+                tempStart.add(Calendar.DAY_OF_YEAR, 1);
+            }
+        } catch (ParseException e) {
+        }
+        System.out.println(days);
+    }
+}
