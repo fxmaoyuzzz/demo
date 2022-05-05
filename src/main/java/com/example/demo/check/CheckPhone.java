35095eb5a5e2274ddffa87d09eafce69c97126d8
lizenghai <fxmaoyuzzz@126.com>
2022-05-05 15:44:23 +0800

    20220505

--- src/main/java/com/example/demo/check/CheckPhone.java
+++ src/main/java/com/example/demo/check/CheckPhone.java
@@ -11,13 +11,20 @@ import java.util.regex.Pattern;
 public class CheckPhone {
 
     public static void main(String[] args) {
-        String phone = "13000000000";
-        Pattern pattern = Pattern.compile("^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$");
-        Matcher match = pattern.matcher(phone);
-        if (!match.matches()){
-            System.out.println(false);
-        }else {
-            System.out.println(true);
-        }
+        //String phone = "13000000000";
+        //Pattern pattern = Pattern.compile("^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$");
+        //Matcher match = pattern.matcher(phone);
+        //if (!match.matches()){
+        //    System.out.println(false);
+        //}else {
+        //    System.out.println(true);
+        //}
+        int a = 10;
+        int b = -5;
+        int i = a + b;
+
+        int i1 = ~(a - 1);
+        System.out.println(i1);
+
     }
 }
